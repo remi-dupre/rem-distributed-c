4cd997743a0fa0c2c86e04502ef2447de1d1b02e
Rémi Dupré <remim.dupre@gmail.com>
2018-04-23 13:25:05 +0200

    Version compatibility on time_ms

--- src/tools.h
+++ src/tools.h
@@ -1,6 +1,11 @@
 /**
  * Some general functions that do not have any place elsewhere.
  */
+#ifndef TOOLS
+#define TOOLS
+
+#define _POSIX_C_SOURCE 200809L
+
 #include <inttypes.h>
 #include <time.h>
 
@@ -9,3 +14,5 @@
  * Return the timestamp, in milliseconds.
  */
 long time_ms();
+
+#endif TOOLS
