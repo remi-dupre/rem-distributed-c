b2ec6858828c2226aec460da8425515e2f351ed9
Rémi Dupré <remim.dupre@gmail.com>
2018-07-04 15:53:05 +0200

    Added tools/build_pdf.sh

---
+++ tools/build_pdf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+to_build="algorithm graphs report"
+
+cd documentation
+mkdir -p pdf
+
+for file in $to_build
+do
+    xelatex $file
+    bibtex $file || true
+    xelatex $file
+    xelatex $file
+
+    cp $file.pdf pdf/ 
+done
